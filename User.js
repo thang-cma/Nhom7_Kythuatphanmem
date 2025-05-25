d451f80c3124a36ef6f3066ab4c0b513db2b312a
thang-cma <23010478@st.phenikaa-uni.edu.vn>
2025-05-25 14:59:07 +0700

    Add files via upload

---
+++ User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  role: { type: String, enum: ['admin', 'user'], default: 'user' }
+});
+
+module.exports = mongoose.model('User', userSchema);
